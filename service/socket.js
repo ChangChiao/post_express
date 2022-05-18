e3c8c5e1e4f08d47ded3a942ac0aa0a8389fecbf
joe <joe.chang1014@gmail.com>
2022-05-18 14:09:24 +0800

    fix socket history

--- service/socket.js
+++ service/socket.js
@@ -1,5 +1,5 @@
 const { Server } = require("socket.io");
-const ChatRoom  = require("../models/chatRoomModel");
+const ChatRoom = require("../models/chatRoomModel");
 module.exports = function (server) {
   const io = new Server(server, {
     path: "/socket.io/",
@@ -9,28 +9,46 @@ module.exports = function (server) {
   });
 
   //建立連接
-  io.of('/chat').on("connection", (socket) => {
+  io.of("/chat").on("connection", (socket) => {
     console.log("socket connect", socket.id);
-    socket.on('joinRoom', function(room) {
+    //加入房間
+    socket.on("joinRoom", (room) => {
       socket.join(room);
     });
-    // socket.join('KPL') //加入房間
     // 監聽 client發來的訊息
-    socket.on("sendMsg", async(msg) => {
+    socket.on("chatMessage", async (msg) => {
       const { message, sender, roomId } = msg;
-      const createdAt = Date.now()
+      const createdAt = Date.now();
       await ChatRoom.findByIdAndUpdate(roomId, {
         $push: { messages: { sender, message, createdAt } },
-      })
-       // socket.to('KPL').emit('talk', msg)
-      socket.emit("receiveMsg", msg);
+      });
+      //針對該房間廣播訊息
+      io.to(roomId).emit("chatMessage", msg);
       console.log(`傳來的訊息`, msg);
     });
+    //歷史訊息
+    socket.on("history", async (info) => {
+      const { lastTime, roomId } = info;
+      let msgList = [];
+      if (lastTime) {
+        msgList = await ChatRoom.find(
+          { _id: roomId },
+          { messages: { $lt: new Date.now(lastTime), $slice: 30 } }
+        );
+      } else {
+        msgList = await ChatRoom.find(
+          { _id: roomId },
+          { messages: { $slice: 30 } }
+        );
+        io.to(roomId).emit("history", msgList);
+      }
+    });
     //斷開連接
-    socket.on("disconnect", () => {});
+    socket.on("disconnect", () => {
+    });
   });
 
-  io.of('/chat').on("connect_error", (err) => {
+  io.of("/chat").on("connect_error", (err) => {
     console.log(`connect_error due to ${err.message}`);
   });
 
