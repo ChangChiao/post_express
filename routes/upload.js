5344f14f44eac15a43b876666fc2a63980c7aaeb
joe <joe.chang1014@gmail.com>
2022-05-21 14:02:13 +0800

    fix bug

--- routes/upload.js
+++ routes/upload.js
@@ -5,7 +5,7 @@ const handleErrorAsync = require("../service/handleErrorAsync");
 const sizeOf = require("image-size");
 const upload = require("../service/image");
 const { ImgurClient } = require("imgur");
-const { isAuth } = require("../service/auth");
+const { isAuth, generateSendJWT } = require("../service/auth");
 router.post(
   "/",
   isAuth,
@@ -15,28 +15,28 @@ router.post(
       return next(appError(400, "尚未上傳檔案", next));
     }
     // const dimensions = sizeOf(req.files[0].buffer);
-    // if (dimensions.width !== dimensions.height) {
-    //   return next(appError(400, "圖片長寬不符合1:1尺寸", next));
+    // if(dimensions.width !== dimensions.height) {
+    //   return next(appError(400,"圖片長寬不符合 1:1 尺寸。",next))
     // }
     const client = new ImgurClient({
       clientId: process.env.IMGUR_CLIENTID,
       clientSecret: process.env.IMGUR_CLIENT_SECRET,
       refreshToken: process.env.IMGUR_REFRESH_TOKEN,
     });
-    console.log("clientId", process.env.IMGUR_CLIENTID);
-    console.log("clientSecret", process.env.IMGUR_CLIENT_SECRET);
-    console.log("refreshToken", process.env.IMGUR_REFRESH_TOKEN);
     const response = await client.upload({
       image: req.files[0].buffer.toString("base64"),
       type: "base64",
       album: process.env.IMGUR_ALBUM_ID,
     });
-    console.log("client", client);
     console.log("response", response);
-    res.status(200).json({
-      status: "success",
-      imgUrl: response.data.link,
-    });
+    if (response.status === 200) {
+      res.status(200).json({
+        status: "success",
+        imgUrl: response.data.link,
+      });
+    } else {
+      next(appError(response.status, response.data, next));
+    }
   })
 );
 
